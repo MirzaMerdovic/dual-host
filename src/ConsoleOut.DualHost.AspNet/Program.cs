a2fa9f704ffa25acfa27bf234b003dcc45c9d575
Mirza Merdovic <mirzamerdovic@gmail.com>
2019-10-22 21:39:27 +0200

    Changes:
    - Refactored DualHost.AspNet.Program.cs parallel task run.

--- src/ConsoleOut.DualHost.AspNet/Program.cs
+++ src/ConsoleOut.DualHost.AspNet/Program.cs
@@ -6,14 +6,17 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleOut.DualHost.AspNet
 {
     internal class Program
     {
-        internal static Task Main(string[] args)
+        internal static async Task Main(string[] args)
         {
+            var source = new CancellationTokenSource();
+
             IHost host = new HostBuilder()
                 .ConfigureHostConfiguration(configHost =>
                 {
@@ -28,12 +31,12 @@ namespace ConsoleOut.DualHost.AspNet
                 .UseConsoleLifetime()
                 .Build();
 
-            var consoleRunner = host.RunAsync();
+            await host.StartAsync(source.Token);
 
             var webHost = HealthCheckServerHost.BuildHost<MyHealthService>();
-            var kestrelRunner = webHost.RunAsync();
+            var kestrelRunner = webHost.RunAsync(source.Token);
 
-            return Task.WhenAll(consoleRunner, kestrelRunner);
+            await host.WaitForShutdownAsync(source.Token);
         }
     }
 }
